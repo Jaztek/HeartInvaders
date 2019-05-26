f962ac6f6b66a06b39d09522447837184ddb9233
Javier López Ureña <javier13333@gmail.com>
2019-05-26 10:52:18 +0200

    Firebase sendMessage and ignore Library

--- Assets/scripts/Firebase/FirebaseController.cs
+++ Assets/scripts/Firebase/FirebaseController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
 
 public class FirebaseController
 {
@@ -39,5 +41,30 @@ public class FirebaseController
     public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
     {
         UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+
+    public void sendMessageTo(string tokenPerdedor, string nombreGanador)
+    {
+        UnityEngine.Debug.Log("---------------------------sendingMessage----------------------------");
+        UnityWebRequest request = new UnityWebRequest("https://fcm.googleapis.com/fcm/send", "POST");
+        request.SetRequestHeader("Authorization", "key=" + "AIzaSyDoicdxWevoAwew0Lky4Uv5laDBmsJEQeY");
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Cache-Control", "no-cache");
+
+        NotificationModel notification = new NotificationModel();
+        notification.title = nombreGanador + " te ha superado fuertemente 👀";
+        notification.body = "Juega o seguirás siendo el último 🤡";
+
+        FirebaseModel firebaseModel = new FirebaseModel();
+        firebaseModel.to = tokenPerdedor;
+        firebaseModel.notification = notification;
+
+        string json = JsonUtility.ToJson(firebaseModel);
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SendWebRequest();
+        UnityEngine.Debug.Log("---------------------------sended----------------------------");
     }
 }
---
+++ Assets/scripts/Models/FirebaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class FirebaseModel
+{
+    public string to;
+    public NotificationModel notification;
+
+}
+
+[Serializable]
+public class NotificationModel
+{
+    public string title;
+    public string body;
+}
